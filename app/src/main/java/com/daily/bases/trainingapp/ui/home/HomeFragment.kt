f778de2bd6d48c446daa4c2a2846b0179eb3f89b
Francisco Silva <franciscosilva22082002@gmail.com>
2025-04-25 15:14:08 -0300

    feat: create home page

--- app/src/main/java/com/daily/bases/trainingapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/daily/bases/trainingapp/ui/home/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.daily.bases.trainingapp.R
 import com.daily.bases.trainingapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,6 +25,17 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val textView: TextView = binding.txtGreeting
+        val name = "Francisco"
+        val greeting = getString(R.string.greeting, name)
+
+        textView.text = greeting
+
+        val trainingRange: TextView = binding.txtTrainingRange
+        val range = "01/01/2025 - 01/04/2025"
+        val rangeString = getString(R.string.training_range_message, range)
+
+        trainingRange.text = rangeString
         return root
     }
 
