8b53fb38a5d3de0fd3b74b67b9bc680f4c5dd0c4
Francisco Silva <franciscosilva22082002@gmail.com>
2025-04-25 10:29:20 -0300

    feat: start drawer activity

--- app/src/main/java/com/daily/bases/trainingapp/MainActivity.kt
+++ app/src/main/java/com/daily/bases/trainingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.daily.bases.trainingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val buttonLogin = findViewById<Button>(R.id.btn_login);
+        buttonLogin.setOnClickListener {
+            handleLogin();
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun handleLogin() {
+        val intent = Intent(this, HomeActivity::class.java);
+        startActivity(intent);
     }
 }
